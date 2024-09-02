05d8a49f3def269b296ef8a97583603e67454909
mackee <macopy123@gmail.com>
2024-09-02 11:22:28 +0900

    fix: fixed an issue where async.Map and Map2 would panic when a break was performed during a loop

--- async/async.go
+++ async/async.go
@@ -1,21 +1,23 @@
 package async
 
 import (
+	"context"
 	"iter"
 	"sync"
 )
 
-// Map applies the function `fn` to each element of the iterator `it`, 
+// Map applies the function `fn` to each element of the iterator `it`,
 // producing a sequence of results of type U.
 //
-// The function `fn` is executed asynchronously for all elements, meaning 
-// that multiple invocations of `fn` may run concurrently on different 
+// The function `fn` is executed asynchronously for all elements, meaning
+// that multiple invocations of `fn` may run concurrently on different
 // elements returned by `it`.
 //
-// The resulting iter.Seq maintains the order of the original iterator `it`, 
+// The resulting iter.Seq maintains the order of the original iterator `it`,
 // ensuring that the output sequence matches the order of the input elements.
 func Map[E any, U any](it iter.Seq[E], fn func(E) U) iter.Seq[U] {
 	return func(yield func(U) bool) {
+		ctx, cancel := context.WithCancel(context.Background())
 		ch := make(chan yieldResult[U])
 		wg := &sync.WaitGroup{}
 		var index int
@@ -24,9 +26,12 @@ func Map[E any, U any](it iter.Seq[E], fn func(E) U) iter.Seq[U] {
 			go func(index int) {
 				defer wg.Done()
 				u := fn(v)
-				ch <- yieldResult[U]{
+				select {
+				case ch <- yieldResult[U]{
 					t:     &u,
 					index: index,
+				}:
+				case <-ctx.Done():
 				}
 			}(index)
 			index++
@@ -36,22 +41,23 @@ func Map[E any, U any](it iter.Seq[E], fn func(E) U) iter.Seq[U] {
 			close(ch)
 		}()
 
-		collectFromChannel(ch, func(t *U) bool {
+		collectFromChannel(ctx, cancel, ch, func(t *U) bool {
 			return yield(*t)
 		})
 	}
 }
 
-// Map2 applies the function `fn` to each pair of elements (V, W) from the iterator `it`, 
+// Map2 applies the function `fn` to each pair of elements (V, W) from the iterator `it`,
 // producing a sequence of pairs of results (V2, W2).
 //
-// The function `fn` is executed asynchronously for all pairs, meaning that multiple 
+// The function `fn` is executed asynchronously for all pairs, meaning that multiple
 // invocations of `fn` may run concurrently on different pairs returned by `it`.
 //
-// The resulting iter.Seq2 maintains the order of the original iterator `it`, ensuring 
+// The resulting iter.Seq2 maintains the order of the original iterator `it`, ensuring
 // that the output sequence of pairs matches the order of the input pairs.
 func Map2[V any, W any, V2 any, W2 any](it iter.Seq2[V, W], fn func(V, W) (V2, W2)) iter.Seq2[V2, W2] {
 	return func(yield func(V2, W2) bool) {
+		ctx, cancel := context.WithCancel(context.Background())
 		type result struct {
 			v2 *V2
 			w2 *W2
@@ -65,9 +71,12 @@ func Map2[V any, W any, V2 any, W2 any](it iter.Seq2[V, W], fn func(V, W) (V2, W
 			go func(index int) {
 				defer wg.Done()
 				v2, w2 := fn(v, w)
-				ch <- yieldResult[result]{
+				select {
+				case ch <- yieldResult[result]{
 					t:     &result{v2: &v2, w2: &w2},
 					index: index,
+				}:
+				case <-ctx.Done():
 				}
 			}(index)
 			index++
@@ -77,7 +86,7 @@ func Map2[V any, W any, V2 any, W2 any](it iter.Seq2[V, W], fn func(V, W) (V2, W
 			close(ch)
 		}()
 
-		collectFromChannel(ch, func(t *result) bool {
+		collectFromChannel(ctx, cancel, ch, func(t *result) bool {
 			return yield(*t.v2, *t.w2)
 		})
 	}
@@ -88,23 +97,28 @@ type yieldResult[T any] struct {
 	index int
 }
 
-func collectFromChannel[T any](ch <-chan yieldResult[T], fn func(t *T) bool) {
+func collectFromChannel[T any](ctx context.Context, cancel func(), ch <-chan yieldResult[T], fn func(t *T) bool) {
+	defer cancel()
 	var currentIndex int
 	m := make(map[int]*T)
 	ret := make(chan *T)
 	go func() {
+		defer close(ret)
 		for yr := range ch {
 			m[yr.index] = yr.t
 			for {
 				if v, ok := m[currentIndex]; ok {
-					ret <- v
+					select {
+					case ret <- v:
+					case <-ctx.Done():
+						return
+					}
 				} else {
 					break
 				}
 				currentIndex++
 			}
 		}
-		close(ret)
 	}()
 
 	for t := range ret {
--- async/async_test.go
+++ async/async_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/mackee/iterutils/async"
 	"github.com/stretchr/testify/assert"
+	"go.uber.org/goleak"
 )
 
 func TestMap(t *testing.T) {
@@ -24,6 +25,27 @@ func TestMap(t *testing.T) {
 	assert.True(t, time.Since(t1) < 1100*time.Millisecond)
 }
 
+func TestMapWithBreak(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
+	ss := slices.Repeat([]string{"a", "b", "c", "d", "e"}, 1000)
+	it := slices.Values(ss)
+	it2 := async.Map(it, func(s string) string {
+		time.Sleep(time.Duration(rand.IntN(11)) * time.Millisecond * 100)
+		return s
+	})
+	t1 := time.Now()
+	ret := make([]string, 0, 100)
+	for v := range it2 {
+		ret = append(ret, v)
+		if len(ret) >= 100 {
+			break
+		}
+	}
+	assert.Equal(t, ss[:100], ret)
+	assert.True(t, time.Since(t1) < 1100*time.Millisecond)
+}
+
 func TestMap2(t *testing.T) {
 	ss := []string{"a", "b", "c", "d", "e"}
 	it := slices.All(ss)
@@ -35,6 +57,27 @@ func TestMap2(t *testing.T) {
 	ret := collect2(it2)
 	assert.Equal(t, ss, ret)
 	assert.True(t, time.Since(t1) < 1100*time.Millisecond)
+}
+
+func TestMap2WithBreak(t *testing.T) {
+	defer goleak.VerifyNone(t)
+
+	ss := []string{"a", "b", "c", "d", "e"}
+	it := slices.All(ss)
+	it2 := async.Map2(it, func(i int, s string) (int, string) {
+		time.Sleep(1 * time.Second)
+		return i, s
+	})
+	t1 := time.Now()
+	ret := make([]string, 0, 3)
+	for _, v := range it2 {
+		ret = append(ret, v)
+		if len(ret) >= 3 {
+			break
+		}
+	}
+	assert.Equal(t, ss[:3], ret)
+	assert.True(t, time.Since(t1) < 3100*time.Millisecond)
 }
 
 func collect2[U any, V any](it iter.Seq2[U, V]) []V {
